128302ce4e906fc984dff26f674640b0d6809c4c
shefcodev <sherefjimoh@gmail.com>
2023-09-24 17:54:04 +0100

    added adduser component data-fetching logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import styles from './AddUser.module.css';
 
 const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+
+    if (enteredUsername && enteredAge) {
+      setEnteredUsername('');
+      setEnteredAge('');
+    }
+  };
+
+  const usernameChangeHandler = ({ target: { value } }) => {
+    setEnteredUsername(value);
+  };
+
+  const ageChangeHandler = ({ target: { value } }) => {
+    setEnteredAge(value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
+        <input id='age' type='number' onChange={ageChangeHandler} />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
